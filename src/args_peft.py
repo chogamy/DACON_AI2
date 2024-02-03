137a60d91b4608c9e2a6318a6296653a88b6be4e
chogamy <gamy0315@daum.net>
2024-02-03 18:25:20 +0900

    add: peft args parser

--- src/args_peft.py
+++ src/args_peft.py
@@ -1,8 +1,28 @@
+import os 
+import re
+import yaml
 
-def parse_train(args, path):
-    print(path)
-    print(args.train)
-
+def parse_peft(args, path):
     
+    file_path = os.path.join(path, f"{args.peft}.yaml")
+    
+    if os.path.isfile(file_path):
+        with open(file_path) as f:
+            args.peft = yaml.full_load(f)
+    else:
+        print(file_path)
+        raise ValueError('Pondering')
+        
+        # e = re.compile('e\d+')
+        # b = re.compile('b\d+')
+        # lr = re.compile('lr\d+e-\d+')
 
-    pass
+        # with open(os.path.join(path, 'basic.yaml')) as f:
+        #     basic = yaml.full_load(f)
+        
+        # basic['num_train_epochs'] = int(e.findall(args.train)[0][1:])
+        # basic['per_device_train_batch_size'] = int(b.findall(args.train)[0][1:])
+        # basic['learning_rate'] = lr.findall(args.train)[0][2:]
+
+        # with open(file_path, 'w') as f:
+        #     yaml.dump(basic, f)      

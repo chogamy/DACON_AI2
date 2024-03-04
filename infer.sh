dff6f3543567c25c1a8a0ed5a940c461d4ee229f
chogamy <gamy0315@daum.net>
2024-03-04 02:37:35 +0000

    fix: args

--- infer.sh
+++ infer.sh
@@ -7,5 +7,5 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
                     --post_train_path /root/DACON_AI2/checkpoints/post_train/maywell/Synatra-42dot-1.3B/ia3 \
                     --train_path /root/DACON_AI2/checkpoints/train/maywell/Synatra-42dot-1.3B/ia3 \
                     --model maywell/Synatra-42dot-1.3B \
-                    --peft lora \
+                    --peft ia3 \
                     --train e3b4lr5e-5 \
